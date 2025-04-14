18bd768460aaba7c2ef5d53bdeed39b1378bee18
Lorelei Barnum <lbarnum@smith.edu>
2025-04-14 08:48:49 -0400

    Added draw_comic function to display our comics in the window

--- R/RPackage.R
+++ R/RPackage.R
@@ -26,3 +26,33 @@ RPackage <- function(number) {
   return(x)
 }
 
+#' @title Plot an xkcd comic strip
+#' @description
+#' 'draw_comic()' display and xkcd comic strip  as aplot in a graphics device (e.g., the RStudio window).
+#' @param x A list object as retruned from [xkcd()]
+#' @importFrom tools file_ext
+#' @importFrom httr GET
+#' @importFrom png readPNG
+#' @importFrom jpeg readJPEG
+#' @importFrom graphics plot.new
+#' @importFrom grid grid.raster
+#' @returns A rastergrob grob. See"Details" section of the [grid::grob()] for more information.
+#' @examples
+#' dino_time<- xkcd(15)
+#' str(dino_time)
+#' @export
+ draw_comic <- function(x) {
+
+  img_type <- tools::file_ext(x$img)
+  tmp <- httr::GET(url = x$img)
+
+  if (img_type == "png") {
+    img <- png::readPNG(tmp$content)
+  } else {
+    img <- jpeg::readJPEG(tmp$content)
+  }
+
+  graphics::plot.new()
+  grid::grid.raster(img)
+}
+
